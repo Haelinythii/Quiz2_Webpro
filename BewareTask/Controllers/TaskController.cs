bcb2b0b13118ee4e4ec7200f8a423efd732076f0
Haelinythii <Haelinythii@users.noreply.github.com>
2020-11-13 19:13:00 +0700

    Add Task

--- BewareTask/Controllers/TaskController.cs
+++ BewareTask/Controllers/TaskController.cs
@@ -26,10 +26,19 @@ namespace BewareTask.Controllers
                         {
                             result += "<tr>" +
                             "<td>" + (x++).ToString() + "</td>" +
-                            "<td>" + a.TaskName + "</td>" +
-                            "<td>" + a.deadline + "</td>" +
-                            "<td>" + "Edit  |   Delete" + "</td>" +
-                            "</tr>";
+                            "<td>" + a.TaskName + "</td>";
+                            if (a.deadline.Year == 1)
+                            {
+                                result +=
+                                "<td>" + "-" + "</td>";
+                            }
+                            else
+                            {
+                                result +=
+                                "<td>" + a.deadline + "</td>";
+                            }
+                            result += "<td>" + "Edit  |   Delete" + "</td>" +
+                                "</tr>";
                         }
                         ViewData["task"] = result;
                         return View();
@@ -41,6 +50,22 @@ namespace BewareTask.Controllers
                 }
             }
             return View();
+        }
+
+        public ActionResult CreateTask(task task)
+        {
+            if (ModelState.IsValid)
+            {
+                using (bewaretaskaspEntities3 database = new bewaretaskaspEntities3())
+                {
+                    int uid = Convert.ToInt32(Session["userID"]);
+                    task.idUser = uid;
+                    database.tasks.Add(task);
+                    database.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }
