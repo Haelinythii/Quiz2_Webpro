d565abac5ae5d82915384af726ea250878be71ef
Neutralix <58196205+neutralix@users.noreply.github.com>
2020-11-13 18:47:11 +0700

    view task list

--- BewareTask/Controllers/TaskController.cs
+++ BewareTask/Controllers/TaskController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BewareTask.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,34 @@ namespace BewareTask.Controllers
         // GET: Task
         public ActionResult Index()
         {
+            if (ModelState.IsValid)
+            {
+                using (bewaretaskaspEntities3 database = new bewaretaskaspEntities3())
+                {
+                    int uid = Convert.ToInt32(Session["userID"]);
+                    var data = database.tasks.Where(c => c.idUser == uid).ToList();
+                    if (data.Count() > 0)
+                    {
+                        string result = "";
+                        int x = 1;
+                        foreach (var a in data)
+                        {
+                            result += "<tr>" +
+                            "<td>" + (x++).ToString() + "</td>" +
+                            "<td>" + a.TaskName + "</td>" +
+                            "<td>" + a.deadline + "</td>" +
+                            "<td>" + "Edit  |   Delete" + "</td>" +
+                            "</tr>";
+                        }
+                        ViewData["task"] = result;
+                        return View();
+                    }
+                    else
+                    {
+                        return View();
+                    }
+                }
+            }
             return View();
         }
     }
